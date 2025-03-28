9ac9c38a0fb593081137afb3d47fcdc743204f06
LuizFelipePSilva <luizfelipep.s@outlook.com.br>
2025-03-28 16:07:32 -0300

    feat: new methods for verification user logged

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,33 +1,23 @@
+// auth.guard.ts
 import { inject } from '@angular/core';
-import {
-  CanActivateFn,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router,
-} from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { CanActivateFn, Router, UrlTree } from '@angular/router';
 import { AuthService, UserRole } from '../service/auth.service';
-export const authGuard: CanActivateFn = (
-  route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot
-): Observable<boolean> => {
+import { map } from 'rxjs/operators';
+
+export const authGuard: CanActivateFn = (route) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
   return authService.verifySession().pipe(
     map((response) => {
-      const userRole: UserRole = response.message;
-      const allowedRoles: UserRole[] = route.data['roles'];
-      if (allowedRoles && allowedRoles.includes(userRole)) {
-        return true;
-      } else {
-        router.navigate(['/login']);
-        return false;
-      }
-    }),
-    catchError((error) => {
-      return of(false);
+      const userRole = response.message;
+      const allowedRoles = route.data['roles'] as UserRole[];
+
+      if (!allowedRoles || allowedRoles.includes(userRole)) return true;
+
+      return router.createUrlTree(['/login'], {
+        queryParams: { returnUrl: router.url },
+      });
     })
   );
 };
