2512b1e18f9fcab4acc548924b72d74263ef2465
LuizFelipePSilva <luizfelipep.s@outlook.com.br>
2025-04-09 17:07:02 -0300

    feat: switch paths of routes because environment

--- src/app/services/auth-services/auth.service.ts
+++ src/app/services/auth-services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment.development';
 
 export type UserRole = 'admin' | 'clerk' | 'employee' | 'user' | null;
 
@@ -10,29 +11,31 @@ export type UserRole = 'admin' | 'clerk' | 'employee' | 'user' | null;
   providedIn: 'root',
 })
 export class AuthService {
-  urlDefaultForProxy = '/v1/api';
+  private readonly apiUrl = environment.apiUrl;
 
   private userRoleSubject = new BehaviorSubject<UserRole>(null);
   user$ = this.userRoleSubject.asObservable();
+
   private loadingSubject = new BehaviorSubject<boolean>(false);
   isLoading$ = this.loadingSubject.asObservable();
 
   constructor(private http: HttpClient, private router: Router) {
     this.verifySession().subscribe();
   }
+
   public getCurrentUserRole(): UserRole {
     return this.userRoleSubject.value;
   }
+
   setLoading(isLoading: boolean) {
     this.loadingSubject.next(isLoading);
   }
 
   verifySession(): Observable<{ message: UserRole }> {
     return this.http
-      .get<{ message: UserRole }>(
-        this.urlDefaultForProxy + '/v1/api/session/verify',
-        { withCredentials: true }
-      )
+      .get<{ message: UserRole }>(`${this.apiUrl}/session/verify`, {
+        withCredentials: true,
+      })
       .pipe(
         tap((response) => {
           this.userRoleSubject.next(response.message);
@@ -47,7 +50,7 @@ export class AuthService {
   login(email: string, password: string): Observable<any> {
     return this.http
       .post(
-        this.urlDefaultForProxy + '/v1/api/session/login',
+        `${this.apiUrl}/session/login`,
         { email, password },
         { withCredentials: true }
       )
@@ -60,11 +63,7 @@ export class AuthService {
 
   logout(): Observable<any> {
     return this.http
-      .post(
-        this.urlDefaultForProxy + '/v1/api/session/logout',
-        {},
-        { withCredentials: true }
-      )
+      .post(`${this.apiUrl}/session/logout`, {}, { withCredentials: true })
       .pipe(
         tap(() => {
           this.userRoleSubject.next(null);
