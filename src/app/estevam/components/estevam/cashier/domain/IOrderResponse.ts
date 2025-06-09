411e65aa6f60bffbcececda660b7ec7730cab98d
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-06-09 17:20:39 -0300

    feat: add new OrderResponse interface

--- src/app/estevam/components/estevam/cashier/domain/IOrderResponse.ts
+++ src/app/estevam/components/estevam/cashier/domain/IOrderResponse.ts
@@ -1,18 +1,23 @@
 export interface IOrderResponse {
-  Comanda: {
-    id: number;
-    Cliente: string;
-    TipoPagamento: string;
-    StatusDoPedido: string;
-    Observaçoes: string | null;
-    HoraDoPedido: Date;
-    ValorTotalDaComanda: number;
-    Produtos: Array<{
-      Produto: string;
-      Quantidade: number;
-      ValorUnitario: number;
-      ValorTotal: number;
-    }>;
-    Mesa: number;
+  id: number;
+  cliente: {
+    nome: string;
+    celular?: string;
   };
+  pagamento: string;
+  status: string;
+  observacoes: string | null;
+  dataPedido: Date;
+  valorTotal: number;
+  produtos: Array<{
+    id: string | undefined; // Ou `string`
+    nome: string;
+    valorUnitario: number;
+    adicionais: Array<{
+      id: string | undefined; // Ou `string`
+      nome: string;
+      valor: number;
+    }> | null;
+  }>;
+  mesa: number | null;
 }
