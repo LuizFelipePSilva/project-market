a3f42490aafc623e789fff982d4b5fec2ab8109e
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-06-09 17:39:25 -0300

    feat: new overlay for delete Product in ListProductComponent

--- src/app/estevam/components/estevam/product/list/product.component.ts
+++ src/app/estevam/components/estevam/product/list/product.component.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { AppComponent } from '../../../../../app.component';
 import { ErrorPopupComponent } from '../../error-popup/error-popup.component';
 import { environment } from '../../../../../../environments/environment.development';
 import { NavbarComponent } from '../../navbar/navbar.component';
+import { CategoryService } from '../../../../services/category-service/category.service';
+import { firstValueFrom } from 'rxjs';
 
 interface IProduct {
   id: string;
   name: string;
   value: number;
-  category: string;
+  categoryId: string;
   description: string;
   status: 'Disponivel' | 'Indisponivel';
+  image: string;
   createdAt: Date;
   updatedAt: Date;
   deletedAt?: Date | null;
@@ -39,15 +41,19 @@ interface IGroupedProducts {
   styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private categoryService: CategoryService
+  ) {}
 
-  title = 'meu-app-cafeteria';
   url_API = '/product';
   init: IProduct[] = [];
   groupedProducts: IGroupedProducts[] = [];
   logado: boolean = true;
   errorMessage: string | null = null;
   showStatusPopup: boolean = false;
+  showDeletePopup: boolean = false;
+  selectedProductDelete: string | null = null;
   selectedProductId: string | null = null;
   desiredAction: 'Disponivel' | 'Indisponivel' | null = null;
   dataSource: IProductPaginate = {
@@ -61,46 +67,65 @@ export class ProductComponent {
   currentPage: number = 1;
   selectedProduct: string | null = null;
 
-  private groupProducts(): IGroupedProducts[] {
-    return this.init
-      .reduce((acc: IGroupedProducts[], product) => {
-        const category = product.category || 'Sem Categoria';
-        const group = acc.find((g) => g.category === category);
-        if (group) {
-          group.products.push(product);
-        } else {
-          acc.push({ category, products: [product] });
-        }
-        return acc;
-      }, [])
-      .sort((a, b) => a.category.localeCompare(b.category));
-  }
-
-  loadProducts(page = 1) {
-    const url = `${environment.apiUrl}${this.url_API}?limit=1000`;
+  async loadProducts(page = 1) {
+    const url = `${environment.apiUrl}${this.url_API}/show?limit=1000`;
     this.http
       .get<IProductPaginate>(url, { withCredentials: true })
-      .subscribe((response) => {
+      .subscribe(async (response) => {
         this.dataSource = response;
         this.currentPage = response.current_page;
         this.init = this.dataSource.data;
-        this.groupedProducts = this.groupProducts();
+
+        const categoriesMap = new Map<string, string>();
+        for (const product of this.init) {
+          if (!categoriesMap.has(product.categoryId)) {
+            const res = await firstValueFrom(
+              this.categoryService.findCategory(product.categoryId)
+            );
+            categoriesMap.set(product.categoryId, res.name);
+          }
+        }
+
+        this.groupedProducts = this.groupProducts(categoriesMap);
       });
   }
 
-  deleteProduct(productId: string) {
-    if (confirm('Tem certeza que deseja excluir este produto?')) {
-      const url = `${environment.apiUrl}${this.url_API}/delete/${productId}`;
-      this.http.delete(url, { withCredentials: true }).subscribe(
-        () => {
-          this.init = this.init.filter((product) => product.id !== productId);
-          this.groupedProducts = this.groupProducts();
-        },
-        (error) => {
-          this.errorMessage = error.error.message;
-        }
-      );
+  private groupProducts(
+    categoriesMap: Map<string, string>
+  ): IGroupedProducts[] {
+    const grouped = new Map<string, IProduct[]>();
+    for (const product of this.init) {
+      const categoryName =
+        categoriesMap.get(product.categoryId) || 'Desconhecido';
+      if (!grouped.has(categoryName)) grouped.set(categoryName, []);
+      grouped.get(categoryName)!.push(product);
     }
+
+    return Array.from(grouped.entries())
+      .map(([category, products]) => ({ category, products }))
+      .sort((a, b) => a.category.localeCompare(b.category));
+  }
+  confirmDelete(productId: string) {
+    this.selectedProductDelete = productId;
+    this.showDeletePopup = true;
+  }
+
+  deleteProduct() {
+    const url = `${environment.apiUrl}${this.url_API}/delete/${this.selectedProductDelete}`;
+    this.http.delete(url, { withCredentials: true }).subscribe(
+      async () => {
+        this.init = this.init.filter(
+          (product) => product.id !== this.selectedProductDelete
+        );
+        const categoriesMap = await this.buildCategoriesMap();
+        this.groupedProducts = this.groupProducts(categoriesMap);
+        this.closeDeletePopup();
+      },
+      (error) => {
+        this.closeDeletePopup();
+        this.errorMessage = error.error.message;
+      }
+    );
   }
   notDisponibilityProduct(productId: string) {
     this.selectedProductId = productId;
@@ -120,13 +145,14 @@ export class ProductComponent {
       this.http
         .patch(url, { status: this.desiredAction }, { withCredentials: true })
         .subscribe(
-          () => {
+          async () => {
             this.init = this.init.map((p) =>
               p.id === this.selectedProductId
                 ? { ...p, status: this.desiredAction! }
                 : p
             );
-            this.groupedProducts = this.groupProducts();
+            const categoriesMap = await this.buildCategoriesMap();
+            this.groupedProducts = this.groupProducts(categoriesMap);
             this.closeStatusPopup();
           },
           (error) => {
@@ -139,6 +165,12 @@ export class ProductComponent {
 
   closeStatusPopup() {
     this.showStatusPopup = false;
+    this.selectedProductId = null;
+    this.desiredAction = null;
+  }
+
+  closeDeletePopup() {
+    this.showDeletePopup = false;
     this.selectedProductId = null;
     this.desiredAction = null;
   }
@@ -170,5 +202,17 @@ export class ProductComponent {
 
   trackByProductId(index: number, product: IProduct): string | undefined {
     return product.id;
+  }
+  private async buildCategoriesMap(): Promise<Map<string, string>> {
+    const map = new Map<string, string>();
+    for (const product of this.init) {
+      if (!map.has(product.categoryId)) {
+        const res = await firstValueFrom(
+          this.categoryService.findCategory(product.categoryId)
+        );
+        map.set(product.categoryId, res.name);
+      }
+    }
+    return map;
   }
 }
