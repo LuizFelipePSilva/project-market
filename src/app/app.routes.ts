b6ae435dce54ccd112fcaec4f20e7e0c79854449
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-06-09 17:17:51 -0300

    refactor: switch estevam routes in app.routes to file estevam.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,95 +1,12 @@
+// routes.ts
 import { Routes } from '@angular/router';
-import { HomeComponent } from './estevam/components/estevam/home/home.component';
-import { ProductComponent } from './estevam/components/estevam/product/list/product.component';
-import { OrderComponent } from './estevam/components/estevam/order/list/order.component';
-import { TableComponent } from './estevam/components/estevam/table/list/table.component';
-import { AuthComponent } from './estevam/components/estevam/auth/login/auth.component';
-import { authGuard } from './estevam/auth/auth.guard';
-import { CreateOrderComponent } from './estevam/components/estevam/order/create-order/create-order.component';
-import { CreateProductComponent } from './estevam/components/estevam/product/create-product/create-product.component';
-import { ReportComponent } from './estevam/components/estevam/report/report.component';
-import { CreateTableComponent } from './estevam/components/estevam/table/create-table/create-table.component';
-import { ListUserComponent } from './estevam/components/estevam/user/list-user/list-user.component';
-import { CashierComponent } from './estevam/components/estevam/cashier/cashier.component';
-import { ConnectWhatsappComponent } from './estevam/components/estevam/admin/connect-whatsapp/connect-whatsapp.component';
 import { HomeBaseComponent } from './shared/home/home.component';
+import { estevamRoutes } from './estevam/estevam.routes';
 
 export const routes: Routes = [
-  {
-    path: 'estevam',
-    children: [
-      {
-        path: 'home',
-        component: HomeComponent,
-        canActivate: [authGuard],
-        data: { roles: ['admin', 'clerk', 'employee', 'user'] },
-      },
-      {
-        path: 'product',
-        component: ProductComponent,
-        canActivate: [authGuard],
-        data: { roles: ['admin'] },
-      },
-      {
-        path: 'product/create',
-        component: CreateProductComponent,
-        canActivate: [authGuard],
-        data: { roles: ['admin'] },
-      },
-      {
-        path: 'order',
-        component: OrderComponent,
-        canActivate: [authGuard],
-        data: { roles: ['admin', 'clerk', 'employee'] },
-      },
-      {
-        path: 'order/create',
-        component: CreateOrderComponent,
-        canActivate: [authGuard],
-        data: { roles: ['admin'] },
-      },
-      {
-        path: 'table',
-        component: TableComponent,
-        canActivate: [authGuard],
-        data: { roles: ['admin', 'clerk', 'employee'] },
-      },
-      {
-        path: 'table/create',
-        component: CreateTableComponent,
-        canActivate: [authGuard],
-        data: { roles: ['admin', 'clerk'] },
-      },
-      {
-        path: 'relatorio',
-        component: ReportComponent,
-        canActivate: [authGuard],
-        data: { roles: ['admin'] },
-      },
-      {
-        path: 'user',
-        component: ListUserComponent,
-        canActivate: [authGuard],
-        data: { roles: ['admin'] },
-      },
-      {
-        path: 'casher',
-        component: CashierComponent,
-        canActivate: [authGuard],
-        data: { roles: ['admin', 'clerk'] },
-      },
-      {
-        path: 'wpp',
-        component: ConnectWhatsappComponent,
-        canMatch: [authGuard],
-        data: { roles: ['admin'] },
-      },
-      { path: 'login', component: AuthComponent },
-      { path: '**', component: HomeBaseComponent },
-    ],
-  },
+  ...estevamRoutes, // Importa todas as rotas do cliente "estevam"
   {
     path: '**',
-    component: HomeBaseComponent,
+    component: HomeBaseComponent, // Rota curinga global
   },
 ];
---
+++ src/app/estevam/estevam.routes.ts
@@ -0,0 +1,120 @@
+// estevam-routes.ts
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/estevam/home/home.component';
+import { authGuard } from './auth/auth.guard';
+import { ProductComponent } from './components/estevam/product/list/product.component';
+import { CreateProductComponent } from './components/estevam/product/create-product/create-product.component';
+import { OrderComponent } from './components/estevam/order/list/order.component';
+import { CreateOrderComponent } from './components/estevam/order/create-order/create-order.component';
+import { TableComponent } from './components/estevam/table/list/table.component';
+import { CreateTableComponent } from './components/estevam/table/create-table/create-table.component';
+import { ReportComponent } from './components/estevam/report/report.component';
+import { ListUserComponent } from './components/estevam/user/list-user/list-user.component';
+import { CashierComponent } from './components/estevam/cashier/cashier.component';
+import { ConnectWhatsappComponent } from './components/estevam/admin/connect-whatsapp/connect-whatsapp.component';
+import { AuthComponent } from './components/estevam/auth/login/auth.component';
+import { HomeBaseComponent } from '../shared/home/home.component';
+import { CreateCategoryComponent } from './components/estevam/category/create-category/create-category.component';
+import { ListCategoryComponent } from './components/estevam/category/list-category/list-category.component';
+import { AdditionalComponent } from './components/estevam/additional/list-additional/additional.component';
+import { CreateAditionalComponent } from './components/estevam/additional/create-aditional/create-aditional.component';
+
+export const estevamRoutes: Routes = [
+  {
+    path: 'estevam',
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin', 'clerk', 'employee', 'user'] },
+      },
+      {
+        path: 'category',
+        component: CreateCategoryComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin', 'clerk'] },
+      },
+      {
+        path: 'category/list',
+        component: ListCategoryComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin', 'clerk'] },
+      },
+      {
+        path: 'additional',
+        component: AdditionalComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin', 'clerk', 'employee', 'user'] },
+      },
+      {
+        path: 'additional/create',
+        component: CreateAditionalComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin'] },
+      },
+      {
+        path: 'product',
+        component: ProductComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin'] },
+      },
+      {
+        path: 'product/create',
+        component: CreateProductComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin'] },
+      },
+      {
+        path: 'order',
+        component: OrderComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin', 'clerk', 'employee'] },
+      },
+      {
+        path: 'order/create',
+        component: CreateOrderComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin'] },
+      },
+      {
+        path: 'table',
+        component: TableComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin', 'clerk', 'employee'] },
+      },
+      {
+        path: 'table/create',
+        component: CreateTableComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin', 'clerk'] },
+      },
+      {
+        path: 'relatorio',
+        component: ReportComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin'] },
+      },
+      {
+        path: 'user',
+        component: ListUserComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin'] },
+      },
+      {
+        path: 'casher',
+        component: CashierComponent,
+        canActivate: [authGuard],
+        data: { roles: ['admin', 'clerk'] },
+      },
+      {
+        path: 'wpp',
+        component: ConnectWhatsappComponent,
+        canMatch: [authGuard],
+        data: { roles: ['admin'] },
+      },
+      { path: 'login', component: AuthComponent },
+      { path: '**', component: HomeBaseComponent },
+    ],
+  },
+];
