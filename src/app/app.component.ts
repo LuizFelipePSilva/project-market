5aabcfb02eef061d2abd01b4a26d5919654f5d0a
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-06-09 17:17:11 -0300

    remove: remove overlay loading to app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,5 @@
 import { Component } from '@angular/core';
-
-import {
-  Router,
-  NavigationStart,
-  NavigationEnd,
-  NavigationCancel,
-  NavigationError,
-  RouterOutlet,
-} from '@angular/router';
-import { Observable } from 'rxjs';
+import { Router, RouterOutlet } from '@angular/router';
 import { AuthService } from './estevam/services/auth-services/auth.service';
 import { CommonModule } from '@angular/common';
 
@@ -19,21 +10,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  isLoading$!: Observable<boolean>;
-
-  constructor(public authService: AuthService, private router: Router) {
-    this.isLoading$ = this.authService.isLoading$;
-
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationStart) {
-        this.authService.setLoading(true);
-      } else if (
-        event instanceof NavigationEnd ||
-        event instanceof NavigationCancel ||
-        event instanceof NavigationError
-      ) {
-        this.authService.setLoading(false);
-      }
-    });
-  }
+  constructor(public authService: AuthService, private router: Router) {}
 }
