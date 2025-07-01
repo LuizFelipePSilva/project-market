1a6932993dee8cd8774634dc8b1be98925151f35
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-07-01 17:52:02 -0300

    feat: add new variable in envrinoment for use in requests

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: '/v1/api/v1/api',
+  adminApiKey: '24b4463c-2266-4dc9-9c61-b8ef72b2f036',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:8080/v1/api',
+  adminApiKey: '24b4463c-2266-4dc9-9c61-b8ef72b2f036',
 };
